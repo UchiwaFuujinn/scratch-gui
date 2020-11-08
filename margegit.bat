d15d8a8780e33e3a7dbd4cc93cf11939ddba97fe
UchiwaFuujinn <uchiwafuujinn@nifty.com>
2020-11-08 11:14:19 +0900

    update

---
+++ margegit.bat
@@ -0,0 +1,11 @@
+git clone git@github.com.uchiwa:UchiwaFuujinn/scratch-gui.git
+git remote add root_branch https://github.com/LLK/scratch-gui.git
+git remote -v
+git fetch root_branch
+git merge root_branch/master
+
+git clone git@github.com.uchiwa:UchiwaFuujinn/scratch-vm.git
+git remote add root_branch https://github.com/LLK/scratch-vm.git
+git remote -v
+git fetch root_branch
+git merge root_branch/master
